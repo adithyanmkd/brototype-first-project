96872f12c70bf583882633d70d3db4b65e46c4ec
adithyan <adithyanmkd123@gmail.com>
2025-05-20 21:06:24 +0530

    google auth modified for production

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `e-commerce.diy/auth/google/callback`,
+      callbackURL:
+        process.env.NODE_ENV === 'production'
+          ? `${process.env.HOST_URL}/auth/google/callback`
+          : 'http://localhost:4000/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
